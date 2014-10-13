0400234b629b4adacc510fc4190699c4924979c4
JohnnyCage <jaguarsy14@gmail.com>
2014-10-13 19:49:28 +0800

    增加新闻列表，设置主面板排版。

--- apps/controller/index.js
+++ apps/controller/index.js
@@ -1,6 +1,6 @@
 var phonecatApp = angular.module('CageStudioApp', []);
 phonecatApp.controller('IndexController', function($scope, $http, $sce) {
-	$http.get('apps/views/music.html').success(function(data) {
+	$http.get('apps/views/news.html').success(function(data) {
 		$scope.partail = data;
 		$scope.partail = $sce.trustAsHtml($scope.partail);
 	});
